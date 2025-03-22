cce205b6770226aac2dc081677608a18f68e2055
Abhilov23 <abhilovgupta01@gmail.com>
2025-03-22 10:06:48 +0530

    added the rectangle shape

--- apps/sketchflow-frontend/app/canvas/[roomId]/page.tsx
+++ apps/sketchflow-frontend/app/canvas/[roomId]/page.tsx
@@ -1,5 +1,7 @@
 "use client"
 import { useEffect, useRef } from "react";
+
+
 export default function Canvas(){
    const canvasRef = useRef<HTMLCanvasElement>(null);
    
@@ -10,11 +12,36 @@ export default function Canvas(){
         const ctx = canvas.getContext("2d");
         
         if(!ctx){
-            console.log("Canvas not supported");
+            
             return;
         }
-        ctx.strokeRect(25, 25, 100, 100);
         
+        let clicked = false;
+        let startX = 0;
+        let startY = 0;
+        
+        canvas.addEventListener("mousedown",(e)=>{
+            clicked = true;
+            startX = e.clientX;
+            startY = e.clientY;
+        })
+        canvas.addEventListener("mouseup",(e)=>{
+            clicked = false;
+            console.log(e.clientX)
+            console.log(e.clientY)
+        })
+
+        canvas.addEventListener("mousemove",(e)=>{
+            if(clicked){
+                const width = e.clientX - startX;
+                const height = e.clientY - startY;
+                ctx.clearRect(0,0,canvas.width, canvas.height);
+                ctx.strokeRect(startX, startY, width, height);
+            }
+        })
+        
+        
+
      }
 
    }, [canvasRef])
